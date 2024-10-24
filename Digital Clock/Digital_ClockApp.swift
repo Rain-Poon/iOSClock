35cfefaa9844bc9509137932136b3780c926743e
Rain-Poon <85403806+Rain-Poon@users.noreply.github.com>
2024-10-24 14:35:13 +1100

    Initial Commit

---
+++ Digital Clock/Digital_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digital_ClockApp.swift
+//  Digital Clock
+//
+//  Created by Rain Poon on 24/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Digital_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
